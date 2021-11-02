fd556fd23cca331caba0cb49e8cb521ce6938296
Wafelack <wafelack@riseup.net>
2021-11-02 11:44:45 +0100

    refactor: package the library and improve overall code

---
+++ polynomials.asd
@@ -0,0 +1,6 @@
+(defsystem "polynomials"
+  :description "Common Lisp library to resolve quadratic polynomials."
+  :version "0.1.0"
+  :author "Wafelack <wafelack@riseup.net>"
+  :licence "LGPL-3.0-or-later"
+  :components ((:file "polynomials")))
--- polynomials.lisp
+++ polynomials.lisp
@@ -1,52 +1,49 @@
-;; Display the variation table for a polynomial
-;; Given the α, the β and 𝑎.
-(defun variations (alpha beta a)
-  (flet ((get-arrow (a f)
-                    (if (= a 0)
-                      "→"
-                      (if (< a 0)
-                        (if f "↗" "↘")
-                        (if f "↘" "↗")))))
-    (format t "   𝑥   | -∞     ~a     +∞~@
-            -------------------------~@
-            Var. 𝑓 |   ~a   ~a   ~a~%" alpha (funcall #'get-arrow a T) beta (funcall #'get-arrow a NIL))))
+(defpackage #:polynomials
+  (:use common-lisp)
+  (:export :variations :canonicalize :extremum :solve :all))
 
-;; Return the discriminant (Δ) given
-;; the terms of a polynomial.
-(defun get-delta (a b c)
+(in-package #:polynomials)
+
+(defun variations (alpha beta a)
+  "Display the variation table for a polynomial given its α, β and 𝑎."
+  (flet ((get-arrow (a f)
+	   (if (= a 0)
+	       "→"
+	       (if (< a 0)
+		   (if f "↗" "↘")
+		   (if f "↘" "↗")))))
+    (format t "   𝑥   | -∞     ~a     +∞~@
+	    -------------------------~@
+	    Var. 𝑓 |   ~a   ~a   ~a~%" alpha (funcall #'get-arrow a T) beta (funcall #'get-arrow a NIL))))
+
+(defun discriminant (a b c)
+  "Get the discriminant of a polynomial."
   (- (* b b) (* 4 a c)))
 
-;; Solve a quadratic equation.
 (defun solve (a b delta)
+  "Solve a quadratic equation given its 𝑎, 𝑏 and Δ."
   (flet ((check (v) (if (subtypep (type-of v) 'complex) NIL v)))
     (let ((left (/ (- (- 0 b) (sqrt delta)) (* 2 a)))
-          (right (/ (+ (- 0 b) (sqrt delta)) (* 2 a))))
+	  (right (/ (+ (- 0 b) (sqrt delta)) (* 2 a))))
       `(,(funcall #'check left) . ,(funcall #'check right)))))
 
-;; Return α and β given the terms of 
-;; a polynomial.
-(defun alphabeta (a b c)
+(defun extremum (a b c)
+  "Return the coordinates of the extermum of a polynomial."
   (flet ((f (x) (+ (* (* x x) a) (* x b) c)))
     (let ((alpha (/ (- 0 b) (* 2 a))))
-      `(,alpha . ,(f alpha)))))
+      (values alpha (f alpha)))))
 
-;; Display the canonic form of a quadratic
-;; polynomial given the α, the β and 𝑎.
 (defun canonicalize (a alpha beta)
+  "Display the canonic form of an equation given its 𝑎 and its extremum coordinates."
   (format t "𝑓: 𝑥 → ~a(𝑥 - ~a)² + ~a~%" a alpha beta))
 
-;; Run all the functions above
-;; and display their results to
-;; the standard output.
 (defun all (a b c)
-  (let* ((_alphabeta (alphabeta a b c))
-         (alpha (car _alphabeta))
-         (beta (cdr _alphabeta))
-         (delta (get-delta a b c))
-         (solved (solve a b delta)))
-    (format t "α = ~a ; β = ~a~%" alpha beta)
-    (canonicalize a alpha beta)
-    (variations alpha beta a)
-    (format t "Δ = ~a~%" delta)
-    (format t "S = {~a, ~a}~%" (car solved) (cdr solved))))
-
+  "Display the canonic form, variations tables and equation solutions of a polynomial."
+  (multiple-value-bind (alpha beta) (extremum a b c)
+    (let* ((delta (discriminant a b c))
+	   (solved (solve a b delta)))
+      (format t "α = ~a ; β = ~a~%" alpha beta)
+      (canonicalize a alpha beta)
+      (variations alpha beta a)
+      (format t "Δ = ~a~%" delta)
+      (format t "S = {~a, ~a}~%" (car solved) (cdr solved)))))
